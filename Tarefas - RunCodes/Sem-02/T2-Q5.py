86c6e355339c524e6c6171a43813ca64cf435b0c
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-03 23:45:04 -0300

    Delete Tarefas - RunCodes/Sem-02/T2-Q5.py

--- Tarefas - RunCodes/Sem-02/T2-Q5.py
+++
@@ -1,9 +0,0 @@
-# A Bate Ponto LTDA bonifica seus funcionários de acordo o tempo de 
-# serviço na empresa Escreva um programa que leia o tempo de serviço 
-# de um funcionário e o valor do bônus por ano trabalhado. Mostre na 
-# tela quanto será a bonificação do funcionário.
-
-Tserv = float(input("Qual o seu tempo de serviço?\n➥ "))
-VbonAn = float(input("E qual o seu bônus anual?\n➥ "))
-
-print(f"Dado o seu tempo de serviço e bbonificação anual, sua bonificação será de {Tserv*VbonAn:.2f}")
