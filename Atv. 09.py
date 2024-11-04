628553f91110abb5e6c8041a7bebc78b1010b72f
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-03 23:33:37 -0300

    Delete Atv. 09.py

--- Atv. 09.py
+++
@@ -1,6 +0,0 @@
-# Seu gato de estimação começou a falar! Mas ele só fala números em Miaulês. 
-# Seria ele um gato-matemático? Peça ao usuário para inserir um número e, 
-# em seguida, imprima esse número multiplicado por “miau”.
-
-num = int(input("Digite um número inteiro: "))
-print(f"\nGato falando:\n{num*"miau"}")
