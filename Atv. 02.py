149feb259b55d6a7a4cd69052949aee014aed596
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-03 23:32:08 -0300

    Delete Atv. 02.py

--- Atv. 02.py
+++
@@ -1,6 +0,0 @@
-cores = {red:="\033[1;31m", green:="\033[1;32m", yeloow:="\033[1;33m", cyano:="\033[1;36m"}
-
-Pnome = input(cyano+"Olá, qual seu primeiro nome? \n➥ ").strip()
-Snome = input(f"E seu sobre nome {Pnome}, qual é? \n➥ ").strip()
-
-print(yeloow+f"\nEste é seu nome e sobre nome\n➥ {Pnome} {Snome}")
