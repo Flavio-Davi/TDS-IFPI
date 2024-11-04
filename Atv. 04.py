e53e6e56602d6d5f78a5c58d375f0a6a6e02f2e9
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-03 23:32:43 -0300

    Delete Atv. 04.py

--- Atv. 04.py
+++
@@ -1,6 +0,0 @@
-print("Calculo: 9x-4x+10")
-x = float(input("\nDigite o valor de X\n➥ "))
-
-res = 9*x - 4*x + 10
-
-res = print(f"\nO resultado do calculo a cima com a váriavel X de valor {x} é {res}")
