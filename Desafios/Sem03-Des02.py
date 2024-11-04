9aa90989c9817c0b79054b9ec43f62fba43a05cc
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-04 02:44:49 -0300

    Create Sem03-Des02.py

---
+++ Desafios/Sem03-Des02.py
@@ -0,0 +1,11 @@
+# Escreva um programa que pergunta ao usuário quantos anos ele tem e, em seguida, 
+# diz a sua idade em anos de cachorro! Você pode calcular a idade da pessoa em anos 
+# de cachorro ao multiplicar sua idade por 7.
+
+print(" IDADE EM ANOS DE CACHORRO ".center(40, "-"))
+
+idade = int(input("\nQual a sua idade?\n➥ "))
+
+print(f"\nSe você fosse um cachorro você teria {idade*7} anos!!")
+print("""'o'______'
+   ||  ||""")
