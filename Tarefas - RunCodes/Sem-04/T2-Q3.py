0821d8cf2e69b4ca124b3e0a0373b4c27a1e612d
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2024-11-10 00:41:23 -0300

    Delete Tarefas - RunCodes/Sem-04/T2-Q3.py

--- Tarefas - RunCodes/Sem-04/T2-Q3.py
+++
@@ -1,8 +0,0 @@
-# Escreva um programa que leia uma temperatura em graus Celsius e mostra na 
-# tela o valor correspondente em graus Fahrenheit
-
-Celsius = float(input())
-
-Fahrenheit = (Celsius * (9 / 5)) + 32
-
-print(Fahrenheit)
