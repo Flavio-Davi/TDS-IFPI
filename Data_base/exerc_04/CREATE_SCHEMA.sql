28505d75b590d26baf8e3c8b6449759ebffbd184
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-05-07 20:13:51 -0300

    Update CREATE_SCHEMA.sql

--- Data_base/exerc_04/CREATE_SCHEMA.sql
+++ Data_base/exerc_04/CREATE_SCHEMA.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA ERP;
 
 CREATE TABLE Projetos(
-	PCODIGO			CHAR(6) NOT NULL, -- Código único do Projeto
+    PCODIGO			CHAR(6) NOT NULL, -- Código único do Projeto
     PNOME			VARCHAR(24), -- Nome Do Projeto
-	DCODIGO			CHAR(3), -- Código do Departamento
+    DCODIGO			CHAR(3), -- Código do Departamento
     RESP			CHAR(6), -- Matrícula do Responsável
     EQUIPE			DEC(5), -- Número de Empregados no Projeto
     DATAINI			DATE, -- Data de início
@@ -15,7 +15,7 @@ CREATE TABLE Projetos(
 );
 
 CREATE TABLE Departamentos(
-	DCODIGO			CHAR(3) NOT NULL, -- Código único do departamento 
+    DCODIGO			CHAR(3) NOT NULL, -- Código único do departamento 
     DNOME			VARCHAR(36) NOT NULL, -- Nome do departamento 
     GERENTE			CHAR(6), -- Matrícula do Gerente
     
@@ -25,16 +25,16 @@ CREATE TABLE Departamentos(
 
 
 CREATE TABLE Empregados(
-	MATR			CHAR(6) NOT NULL, -- Matricula única do empregado 
-	NOME			VARCHAR(12) NOT NULL, -- Primeiro name
-	SOBRENOME 		VARCHAR(15) NOT NULL, 
+    MATR			CHAR(6) NOT NULL, -- Matricula única do empregado 
+    NOME			VARCHAR(12) NOT NULL, -- Primeiro name
+    SOBRENOME 		        VARCHAR(15) NOT NULL, 
     DEPT 			CHAR(3), -- Código de departamento do empregado
     FONE 			CHAR(14),
     DATAADM			DATE,  -- Data de admissão
     CARGO			CHAR(10), -- Cargo do empregado
-    NIVELEDUC		DEC,
+    NIVELEDUC		        DEC,
     SEXO			ENUM("F", "M"),
-    DATANASC		DATE,
+    DATANASC		        DATE,
     SALARIO			DECIMAL(9, 2), -- Salário Anual
     BONUS 			DECIMAL(9, 2),-- Bônus Anual
     COMIS 			DECIMAL(9, 2),-- Comissão Anual
