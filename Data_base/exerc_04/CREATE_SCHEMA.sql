4ca75ac3909946d0661900293d5d67895023a3fb
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-05-07 20:30:18 -0300

    Update CREATE_SCHEMA.sql

--- Data_base/exerc_04/CREATE_SCHEMA.sql
+++ Data_base/exerc_04/CREATE_SCHEMA.sql
@@ -9,9 +9,7 @@ CREATE TABLE Projetos(
     DATAINI			DATE, -- Data de início
     DATAFIM			DATE, -- Data do final
     
-    PRIMARY KEY (PCODIGO),
-    FOREIGN KEY (DCODIGO) REFERENCES Departamentos(DCODIGO),
-    FOREIGN KEY (RESP) REFERENCES Empregados(MATR)
+    PRIMARY KEY (PCODIGO)
 );
 
 CREATE TABLE Departamentos(
@@ -19,26 +17,29 @@ CREATE TABLE Departamentos(
     DNOME			VARCHAR(36) NOT NULL, -- Nome do departamento 
     GERENTE			CHAR(6), -- Matrícula do Gerente
     
-    PRIMARY KEY (DCODIGO),
-    FOREIGN KEY (GERENTE) REFERENCES Empregados(MATR)
+    PRIMARY KEY (DCODIGO)
 );
 
 
 CREATE TABLE Empregados(
     MATR			CHAR(6) NOT NULL, -- Matricula única do empregado 
     NOME			VARCHAR(12) NOT NULL, -- Primeiro name
-    SOBRENOME 		        VARCHAR(15) NOT NULL, 
+    SOBRENOME     		VARCHAR(15) NOT NULL, 
     DEPT 			CHAR(3), -- Código de departamento do empregado
     FONE 			CHAR(14),
     DATAADM			DATE,  -- Data de admissão
     CARGO			CHAR(10), -- Cargo do empregado
-    NIVELEDUC		        DEC,
+    NIVELEDUC	        	DEC,
     SEXO			ENUM("F", "M"),
     DATANASC		        DATE,
     SALARIO			DECIMAL(9, 2), -- Salário Anual
     BONUS 			DECIMAL(9, 2),-- Bônus Anual
     COMIS 			DECIMAL(9, 2),-- Comissão Anual
 
-	PRIMARY KEY (MATR),
-	FOREIGN KEY (DEPT) REFERENCES Departamentos(DCODIGO)
-)
+	PRIMARY KEY (MATR) 
+);
+
+ALTER TABLE Departamentos ADD FOREIGN KEY (GERENTE) REFERENCES Empregados(MATR);
+ALTER TABLE Empregados ADD FOREIGN KEY (DEPT) REFERENCES Departamentos(DCODIGO);
+ALTER TABLE Projetos ADD FOREIGN KEY (DCODIGO) REFERENCES Departamentos(DCODIGO);
+ALTER TABLE Projetos ADD FOREIGN KEY (RESP) REFERENCES Empregados(MATR);
