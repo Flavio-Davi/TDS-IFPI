2a938a80bcb3b6a78bf0836a4bccd1af4afd71bb
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-05-28 00:35:56 -0300

    Update exerc_04.py

--- POO/exerc_04.py
+++ POO/exerc_04.py
@@ -2,7 +2,7 @@ from re import search
 from os import name
 from os import system
 from time import sleep
-from datetime import datetime
+import datetime 
 
 cor = {"vermelho": "\033[1;31m", "verde": "\033[1;32m", "amarelo": "\033[1;33m", 
         "azul": "\033[1;34m", "roxo": "\033[1;35m", "cyano": "\033[1;36m"}
@@ -39,40 +39,60 @@ def menu():
 class Boleto:
     def __init__(self):
         self.data_base = {}
-        self.id_boleto = 0
+        self._id_boleto = 0
         self.STATUS = ["EM ABERTO", "PAGO", "VENCIDO", "CANCELADO"]
-        self.data_atual = datetime.today().strftime("%d/%m/%Y")
+        self.data_atual = datetime.datetime.strptime(datetime.datetime.today().strftime("%d/%m/%Y"), "%d/%m/%Y").date
+
+    @property
+    def id_boleto(self):
+        return self._id_boleto
 
 
-    def criar(self, nome: str, data_emissao: datetime, data_venc: datetime):
+    def valor_atual(self, id_boleto: int):
+        """Desconto de 5% se pago até 3 dias antes do vencimento.
+        Multa de 2% + juros de 0,03% ao dia se pago após o vencimento."""
+
+        if id_boleto not in self.data_base:
+            raise ValueError(">>> ID não encontrado, verificar e tentar novamente.")
+        
+        data_venc = self.data_base[id_boleto][4]
+        if isinstance(data_venc, str):
+            data_venc = datetime.datetime.strptime(data_venc, "%d/%m/%Y").date()
+        
+        return (data_venc-self.data_atual).days
+
+
+    def criar(self, nome: str, valor: float, data_emissao: datetime, data_venc: datetime):
         data = []
-        self.id_boleto += 1
+        self._id_boleto += 1
 
         data.append(self.STATUS[0])
         data.append(nome)
+        data.append(valor)
         data.append(data_emissao)
         data.append(data_venc)
+        data.append("")
         self.data_base[self.id_boleto] = data
 
         return f"\n>>> Boleto número {self.id_boleto} criado com sucesso."
 
 
     def consulta(self):
-        print("ID boleto\t\tInformações")
+        print(cor["azul"]+f"{'ID':<10}{'STATUS':<13}{'NOME':<16}{'VALOR R$':<19}{'DATA EMISSÃO':<22}{'DATA VENCIMENTO':<25}{'REGISTRO DE PAGAMENTO':<28}")
         for key, value in self.data_base.items():
-            print(f"{key:<20}{value}")
+            print(cor["cyano"]+f"{key:<10}{value[0]:<13}{value[1]:<16}{value[2]:<19}{value[3]:<22}{value[4]:<25}{value[5]:<28}")
         
         return input("\nPressione enter para voltar.")
 
 
-    def excluir(self, id_boleto: int):
+    def cancelar(self, id_boleto: int):
         verificar = False
         for key, value in self.data_base.items():
             if key == id_boleto:
                 verificar = True
         if verificar:
-            self.data_base.pop(id_boleto)
-            return f"\n>>> Boleto de ID {id_boleto}, excluido com sucesso."
+            self.data_base[id_boleto][0] = "CANCELADO"
+            return f"\n>>> Boleto de ID {id_boleto}, cancelado com sucesso."
         else:
             return f"\nBoleto de ID {id_boleto} não localizado, verifique e tente novamente."
 
@@ -85,6 +105,7 @@ class Boleto:
         if verificar == True:
             if search("EM ABERTO", self.data_base[id_boleto][0]):
                 self.data_base[id_boleto][0] = "PAGO"
+                self.data_base[id_boleto][5] = self.data_atual
                 return ">>> Boleto pago com sucesso."
             else:
                 return f">>> O boleto de ID {id_boleto}, não está com status EM ABERTO.\nContatar o emissor."
@@ -94,27 +115,33 @@ class Boleto:
 
 if __name__ == '__main__':
     i = Boleto()
-    
+
     while True:
         iniciar = menu()
 
         if iniciar == "1":
             nome = input("Nome do devedor: ")
+            valor = input("Valor do boleto: R$")
             data_emissao = input("Data da emissão: ")
             data_venc = input("Data de vencimento: ")
 
-            print(cor["verde"]+i.criar(nome, data_emissao, data_venc))
+            print(cor["verde"]+i.criar(nome, valor, data_emissao, data_venc))
             sleep(2)
+
         if iniciar == "2":
             i.consulta()
+
         if iniciar == "3":
             id_boleto = int(input("Digite o ID do boleto que deseja excluir: "))
-            print(i.excluir(id_boleto))
+            print(i.cancelar(id_boleto))
             sleep(1)
+        
         if iniciar == "4":
             id_pagar = int(input("Digite o ID do boleto que deseja pagar: "))
+            print(i.valor_atual(id_pagar))
             print(i.pagar(id_pagar))
             sleep(2)
+        
         if iniciar == "5":
             limpar()
             print(">>> Programa encerrado.")
