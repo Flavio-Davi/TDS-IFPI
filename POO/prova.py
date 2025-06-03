442dd8c3b5950f301f5282efe16acc9dd40952aa
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-06-02 23:11:36 -0300

    Update prova.py

--- POO/prova.py
+++ POO/prova.py
@@ -1,6 +1,7 @@
-# Ludimila ANdrade Maciel & Flávio Davi da Silva Alves
+# Ludimila Andrade Maciel & Flávio Davi da Silva Alves
 
 import datetime
+import json
 from os import system, name
 from time import sleep
 
@@ -35,32 +36,65 @@ def menu():
             break
     return user
 
+def pedir_data(msg):
+    while True:
+        try:
+            limpar()
+            print(msg.center(50, "#"))
+            d = int(input("dia: "))
+            m = int(input("mês: "))
+            a = int(input("ano: "))
+            return datetime.date(a, m, d)
+        except:
+            print("Data inválida.")
+            sleep(1)
+
 class Reserva:
     def __init__(self):
         self._id = 0
         self._tipo_quarto = {"SIMPLES": 150.0, "DUPLO": 250.0, "LUXO": 400.0}
         self.reservas = {}
+        self.carregar()
 
     @property
     def tipo_quarto(self):
         return self._tipo_quarto
+
+    def salvar(self):
+        with open("reservas.json", "w") as f:
+            json.dump({"id": self._id, "dados": self.reservas}, f, default=str)
+
+    def carregar(self):
+        try:
+            with open("reservas.json", "r") as f:
+                data = json.load(f)
+                self._id = data["id"]
+                for k, v in data["dados"].items():
+                    v[2] = datetime.datetime.strptime(v[2], "%Y-%m-%d").date()
+                    v[3] = datetime.datetime.strptime(v[3], "%Y-%m-%d").date()
+                    self.reservas[int(k)] = v
+        except:
+            self._id = 0
+            self.reservas = {}
 
     def reservar(self, nome, tipo_qt, data_checkIN, data_checkOUT):
         self._id += 1
         qtd_diaria = (data_checkOUT - data_checkIN).days
         valor_total = qtd_diaria * self.tipo_quarto[tipo_qt]
         self.reservas[self._id] = [nome, tipo_qt, data_checkIN, data_checkOUT, qtd_diaria, "ATIVA", valor_total]
+        self.salvar()
 
     def visualizar(self):
         if not self.reservas:
             print("Nenhuma reserva encontrada.")
             return
         for k, v in self.reservas.items():
-            print(f"{k}: {v}")
+            print(f"ID: {k} | Nome: {v[0]} | Quarto: {v[1]} | Check-in: {v[2]} | Check-out: {v[3]} | Diárias: {v[4]} | Total: R${v[6]:.2f} | Status: {v[5]}")
 
     def cancelar(self, reserva_id):
         if reserva_id in self.reservas and self.reservas[reserva_id][5] == "ATIVA":
             self.reservas[reserva_id][5] = "CANCELADA"
+            self.salvar()
             return True
         return False
 
@@ -73,18 +107,19 @@ class Reserva:
             self.reservas[reserva_id][3] = novo_checkOUT
             self.reservas[reserva_id][4] = qtd_diaria
             self.reservas[reserva_id][6] = valor_total
+            self.salvar()
             return True
         return False
 
     def checkin(self, reserva_id):
         if reserva_id in self.reservas and self.reservas[reserva_id][5] == "ATIVA":
             self.reservas[reserva_id][5] = "CONCLUIDA"
+            self.salvar()
             return True
         return False
 
 if __name__ == '__main__':
     i = Reserva()
-
     while True:
         user = menu()
         hoje = datetime.date.today()
@@ -100,23 +135,17 @@ if __name__ == '__main__':
                     tipo_qt = ["SIMPLES", "DUPLO", "LUXO"][int(tipo_qt) - 1]
                     break
 
-            limpar()
-            print(" DATA DE CHECK-IN ".center(50, "#"))
-            dia = int(input("dia: "))
-            mes = int(input("mês: "))
-            ano = int(input("ano: "))
-            data_checkIN = datetime.date(ano, mes, dia)
+            data_checkIN = pedir_data(" DATA DE CHECK-IN ")
             if data_checkIN < hoje:
-                raise Exception("Data de check-in inválida.")
+                print("Data de check-in inválida.")
+                sleep(2)
+                continue
 
-            limpar()
-            print(" DATA DE CHECK-OUT ".center(50, "#"))
-            dia = int(input("dia: "))
-            mes = int(input("mês: "))
-            ano = int(input("ano: "))
-            data_checkOUT = datetime.date(ano, mes, dia)
+            data_checkOUT = pedir_data(" DATA DE CHECK-OUT ")
             if data_checkOUT <= data_checkIN:
-                raise Exception("Data de check-out inválida.")
+                print("Data de check-out inválida.")
+                sleep(2)
+                continue
 
             i.reservar(nome, tipo_qt, data_checkIN, data_checkOUT)
             print("Reserva realizada com sucesso.")
@@ -140,16 +169,8 @@ if __name__ == '__main__':
             i.visualizar()
             try:
                 reserva_id = int(input("ID da reserva para alterar: "))
-                print(" NOVA DATA DE CHECK-IN ".center(50, "#"))
-                dia = int(input("dia: "))
-                mes = int(input("mês: "))
-                ano = int(input("ano: "))
-                novo_checkIN = datetime.date(ano, mes, dia)
-                print(" NOVA DATA DE CHECK-OUT ".center(50, "#"))
-                dia = int(input("dia: "))
-                mes = int(input("mês: "))
-                ano = int(input("ano: "))
-                novo_checkOUT = datetime.date(ano, mes, dia)
+                novo_checkIN = pedir_data(" NOVA DATA DE CHECK-IN ")
+                novo_checkOUT = pedir_data(" NOVA DATA DE CHECK-OUT ")
                 if novo_checkOUT <= novo_checkIN or novo_checkIN < hoje:
                     print("Datas inválidas.")
                 elif i.alterar(reserva_id, novo_checkIN, novo_checkOUT):
@@ -165,4 +186,14 @@ if __name__ == '__main__':
             i.visualizar()
             try:
                 reserva_id = int(input("ID da reserva para check-in: "))
-                if i.check
+                if i.checkin(reserva_id):
+                    print("Check-in realizado.")
+                else:
+                    print("Não foi possível realizar check-in.")
+            except:
+                print("ID inválido.")
+            sleep(2)
+
+        elif user == "5":
+            print("Saindo...")
+            break
