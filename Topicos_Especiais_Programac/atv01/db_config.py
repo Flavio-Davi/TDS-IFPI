b1e3741100b0bb43e5df19de4c75a383032465ae
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-09-01 01:45:17 -0300

    Update db_config.py

--- Topicos_Especiais_Programac/atv01/db_config.py
+++ Topicos_Especiais_Programac/atv01/db_config.py
@@ -30,14 +30,12 @@ class Conexao:
                 print(f"ERROR: Banco de dados *{DB_NAME}* inexistente.")
             else:
                 print(f"ERROR: {e}")
-            
+
 
     def execute_query_read(self, query, param=None):
         try:
             self._cursor.execute(query, param)
             result = self._cursor.fetchall()
-            self._cursor.close()
-            self._cnx.close()
         except mysql.connector.Error as e:
             print(f"ERROR: {e}")
         return result
@@ -47,11 +45,16 @@ class Conexao:
         try:
             self._cursor.execute(query, param)
             self._cnx.commit()
-            self._cursor.close()
-            self._cnx.close()
         except mysql.connector.Error as e:
             return f"ERROR: {e}"
         return True
+
+
+    def close_connection(self):
+        self._cursor.close()
+        self._cnx.close()
+
+        return "Conexão encerrada"
 
 
 if __name__ == '__main__':
